0d000a00ef5621fedf3c82ab182a61282a680440
htgy <hhhtgy@qq.com>
2025-11-22 02:39:07 +0800

    Update ss-all.sh

--- ss-all.sh
+++ ss-all.sh
@@ -41,7 +41,7 @@ libsodium_file="libsodium-1.0.20"
 libsodium_url="https://github.com/jedisct1/libsodium/releases/download/1.0.20-RELEASE/libsodium-1.0.20.tar.gz"
 
 mbedtls_file="mbedtls-2.16.0"
-mbedtls_url="https://tls.mbed.org/download/mbedtls-2.16.0-gpl.tgz"
+mbedtls_url="https://github.com/Mbed-TLS/mbedtls/archive/mbedtls-2.16.0.tar.gz"
 
 shadowsocks_python_file="shadowsocks-master"
 shadowsocks_python_url="https://github.com/shadowsocks/shadowsocks/archive/master.zip"
