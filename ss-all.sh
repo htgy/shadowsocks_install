971be9d4cc17d12cb82a542127901b03ec2f9da3
htgy <hhhtgy@qq.com>
2025-11-23 02:02:13 +0800

    Update ss-all.sh

--- ss-all.sh
+++ ss-all.sh
@@ -566,7 +566,7 @@ install_dependencies(){
         done
     elif check_sys packageManager apt; then
         apt_depends=(
-            gettext build-essential unzip gzip curl openssl libssl-dev
+            gettext build-essential unzip gzip curl openssl libssl-dev python-is-python3
             autoconf automake libtool gcc make perl cpio libpcre3 libpcre3-dev zlib1g-dev libev-dev libc-ares-dev git qrencode
         )
 
